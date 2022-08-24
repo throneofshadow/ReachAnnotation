98d07ba16c89a604cbef3efe6bb73a5841548866
throneofshadow <brrnelson@berkeley.edu>
2022-08-24 12:50:11 -0700

    Added structure, filepath functions for gui.

---
+++ reachannotate/reachannotate.py
@@ -0,0 +1,7 @@
+import utils.interface as interface
+import utils.widgets as widgets
+
+
+class ReachAnnotate:
+    def __init__(self, directory_path_videos):
+        self.video_directory_path = directory_path_videos
