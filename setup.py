9954f5f7290858fc1f7059e2ab756d1dd3d1e37c
Brett Nelson <43356326+throneofshadow@users.noreply.github.com>
2022-10-07 11:32:48 -0700

    wrong name in package...

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("Readme.md", 'r') as f:
 
 
 setup(
-    name='ReachSample',
+    name='ReachAnnotation',
     version='0.1.0',
     description=
     'A library intended to generate a GUI to annotate / label video files for the ReachMaster experimental'
