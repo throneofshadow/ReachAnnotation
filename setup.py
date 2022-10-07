ebfa41e5b709a3b6cd72b06a4c5f28ed6cac5c53
Brett Nelson <43356326+throneofshadow@users.noreply.github.com>
2022-10-07 10:14:18 -0700

    UD

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 from setuptools import setup
 
+with open("Readme.md", 'r') as f:
+    long_description = f.read()
+
+
 setup(
     name='ReachSample',
     version='0.1.0',
-    description='A python library for sampling commands within a robotic workspace, '
-                'compatible with the ReachMaster experimental paradigm.',
+    description=
+    'A library intended to generate a GUI to annotate / label video files for the ReachMaster experimental'
+    ' paradigm.Install on python using pip install ReachAnnotate -- Current published version is 0.1.0',
     url='https://github.com/throneofshadow/ReachAnnotation',
-    author='Brett Nelson',
+    author='Brett Nelson, Nicholas Chin',
     author_email='bnelson@lbl.gov',
     license='BSD-3-Clause-LBNL',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['tkinter', 'video', 'player', 'video player', 'tkvideoplayer', 'play video in tkinter', 'custom annotation'],
     packages=['ReachAnnotation'],
-    install_requires=['pandas', 'tkinter', 'matplotlib.pyplot', 'unittest',
-                      'numpy', 'os', 'pdb', 'ffmpeg'],
+    install_requires=['pandas', 'tkinter', 'unittest', 'os', 'av', 'pillow'],
+    include_package_data=True,
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
@@ -20,6 +29,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.6',
     ],
 )
