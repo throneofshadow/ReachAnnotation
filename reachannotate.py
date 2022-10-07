971271468cf66ed21f1751c749bbf6fd2b893182
Brett Nelson <43356326+throneofshadow@users.noreply.github.com>
2022-10-07 11:11:57 -0700

    UD

--- reachannotate.py
+++ reachannotate.py
@@ -7,7 +7,7 @@ from tkinter import filedialog
 from tkinter import *
 from tkinter import ttk
 import pandas as pd
-from reachannotation_gui import ReachAnnotation
+from reachannotate.reachannotation_gui import ReachAnnotation
 
 
 def update_duration(event):
